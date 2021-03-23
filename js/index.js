22dc77c6a4445a0032ad492f6f64c1e75a0547dc
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2021-03-23 19:08:22 +0000

    updated chart to visualise problem better

--- js/index.js
+++ js/index.js
@@ -15,14 +15,18 @@ var groupColours = {
   10 : "#82204a",
   
 }
+
+var chartdata;
+
 window.onload = () => {
   KeyLines.promisify();
   KeyLines.create({container: chartDivId}).then((loadedChart) => {
     chart = loadedChart;
     getDataPromise().then((data) => {
-      chart.load(data);
+      chartdata = data;
+      chart.load(chartdata);
       chart.layout('standard');
-      // chart.layout('sequential', {level: 'group'})
+      // chart.layout('sequential', {level: 'total'})
     }).catch((error)=>{
       console.log("Failed getting JSON data!");
     })
@@ -41,20 +45,56 @@ function getDataPromise(){
       // Reformat data for keylines
       var itemsNodes = [];
       var idNumber=0;
+      nodeDegrees = [];
 
       // Each node is added and given a colour
       data.nodes.forEach(node => {
-        console.log(node);
-          itemsNodes.push({ type: 'node', id: "n"+idNumber, t: node.name, c:groupColours[node.group], d :{group:node.group}});
+          itemsNodes.push({ 
+            type: 'node', 
+            id: idNumber, 
+            t: node.name, 
+            c:groupColours[node.group], 
+            d :{
+              group:node.group,
+              indegree:0,
+              outdegree:0,
+              total:0
+            },
+            e : 1
+          });
+          nodeDegrees[idNumber] = {in:0,out:0};
           idNumber+=1;
       });
+
       // Each edge is added
       idNumber = 0;
       data.links.forEach(link => {
-        itemsNodes.push({ type: 'link', id: "l"+idNumber, id1 : "n"+link.source, id2: "n"+link.target, w:link.value});
+        itemsNodes.push({ 
+          type: 'link', 
+          id: "l"+idNumber, 
+          id1 : link.source, 
+          id2: link.target, 
+          d : {
+            value : link.value
+          },
+          t:link.value,
+          fs:50
+        });
+        // update in and out degree of nodes
+        nodeDegrees[link.source].out+=1;
+        nodeDegrees[link.target].in+=1;
         idNumber+=1;
       });
 
+      // update the nodes in the dictionary
+      itemsNodes.forEach(item => {
+       if(item.type=="node"){
+        item.d.indegree=nodeDegrees[item.id].in;
+        item.d.outdegree=nodeDegrees[item.id].out;
+        item.d.total = item.d.outdegree+item.d.indegree;
+        item.e = item.d.total;
+      }
+    });
       keylinesData.items = itemsNodes;
       resolve(keylinesData);
   });
